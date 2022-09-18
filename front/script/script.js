69dbecda330fb82a56d4024c50e5d973b3393370
n1colass <atelando14@gmail.com>
2022-09-18 14:23:15 +0300

    Test Add Class

--- front/script/script.js
+++ front/script/script.js
@@ -1,8 +1,8 @@
 const API_PRODUCT = 'data/products.json';
 
-const img = document.querySelector('.image-product');
+/* const img = document.querySelector('.image-product');
 const description = document.querySelector('.description-product');
-const price = document.querySelector('.price-product');
+const price = document.querySelector('.price-product'); */
 
 const getProduct = async (url) => {
   const response = await fetch(url);
@@ -19,4 +19,13 @@ getProduct(API_PRODUCT).then((data) => {
   price.innerText = `${data[0].price}₴`;
 });
 
+// NEW VERSION CREATES CARDS FOR CLASS
 const cardWrapper = document.querySelector('.cards__wrapper');
+
+class Cards {
+  constructor(image, description, price) {
+    this.image = image;
+    this.description = description;
+    this.price = price;
+  }
+}
